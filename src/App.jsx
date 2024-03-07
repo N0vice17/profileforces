f539eb606439e4eb914f15e9ff38357b9c4a040a
Debojit <debojitganguly69@gamil.com>
2024-03-07 22:21:57 +0530

    revamping the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Navbar from "./Navbar/Navbar.jsx"
 import Home from "./Home/Home.jsx"
-import Problemgraph from "./Problems/Problemgraph.jsx"
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
-      <Problemgraph />
     </>
   )
 }
